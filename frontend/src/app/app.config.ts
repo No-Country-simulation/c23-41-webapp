5058ea327097511dc7f324fd1061a2ba12cce9dd
LuEsc <luisluna338@gmail.com>
2025-01-30 22:25:19 -0500

    ref: delete hidratation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes),
-    provideAnimations(),  provideClientHydration()]
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimations()]
 };
