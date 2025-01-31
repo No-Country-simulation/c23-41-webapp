ca0cd6207c6d625a507b7f708188095be6dee490
Pablo <pablo.and.ramirez@outlook.com>
2025-01-31 09:08:42 -0300

    first backend test commit

--- monorepo/backend/src/main/java/com/skillio/api_v1/repository/admin/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skillio.api_v1.repository.admin;
-
-import com.skillio.api_v1.domain.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AdminRepository extends JpaRepository<Admin, UUID> {
-}
