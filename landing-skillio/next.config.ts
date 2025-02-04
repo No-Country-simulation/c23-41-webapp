1f4f3ee2d03ac7724aa988645b44bd205936b727
LuEsc <luisluna338@gmail.com>
2025-02-03 22:21:55 -0500

    title in skillio app

--- landing-skillio/next.config.ts
+++ landing-skillio/next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   /* config options here */
    output: 'export',  // For static export
    distDir: 'out',
 
    // Additional optimizations for Netlify
   trailingSlash: true,  // Helps with static export routing
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
